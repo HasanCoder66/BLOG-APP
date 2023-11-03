62a04ff0db9af9ce114cd1ffd6760c8b59c0856e
66CoderHasan <hasanashraf874@gmail.com>
2023-11-03 22:33:39 +0500

    in process

--- src/components/blog/BlogView.jsx
+++ src/components/blog/BlogView.jsx
@@ -1,18 +1,48 @@
-import React from 'react'
-import './blogview.css'
+import React from "react";
+import "./blogview.css";
+import { useRef } from "react";
 function BlogView() {
+
+  const user = useRef()
+  const blogcontent = useRef()
+  const blogdescription = useRef()
+  const blogtitle = useRef()
   return (
     <>
-    <div className='perblog'>   
-            <p className='perbloguser'>adasfj</p>
-            <div className="blogcontent">
-                hasan ka blog post
-            <h1 className='perblogtitle'> maha pan</h1>
-            <p className='perblogbody'> ashhad maha </p>
-            </div>
-        </div>
-</>
-  )
+      {/* <div className="perblog">
+        <p className="perbloguser">adasfj</p>
+        <div className="blogcontent">
+          hasan ka blog post
+          <h1 className="perblogtitle"> maha pan</h1>
+          <p className="perblogbody"> ashhad maha </p>
+        </div>
+      </div> */}
+       <input
+              placeholder="User Name"
+              type="text"
+              className="loginInput"
+              ref={user}
+              />
+            <input
+              placeholder="Blog Content"
+              ref={blogcontent}
+              type="text"
+              className="loginInput"
+              />
+            <input
+              ref={blogtitle}
+              placeholder=" Blog Title"
+              type="text"
+              className="loginInput"
+              />
+            <input
+              ref={blogdescription}
+              placeholder="Blog description "
+              type="text"
+              className="loginInput"
+            />
+    </>
+  );
 }
 
-export default BlogView
+export default BlogView;
--- src/components/blog/Navbar.jsx
+++ src/components/blog/Navbar.jsx
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 function Navbar() {
+  const [modal , setModal] = useState(false)
+
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  }
+
+  const closeModal = () => {
+    setModalOpen(false);
+  }
+  
   return (
     <div style={{backgroundColor:'##9862fe'}} className="container">
       <div className="header">
         <Link className="headertitle" to={"/"}>
           <h2>Blogs</h2>
         </Link>
-        <Link to={"/create"}>
-          <button className="button">Create a Blog</button>
+        <Link >
+          <button className="button" onClick={ () => setModal(true)}>Create a Blog</button>
         </Link>
-            
+      </div>
+    </div>
+  );
+  
+}
+
+export default Navbar;
+
+
+
+
+function Modal({ isOpen, onClose, children }) {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal">
+        <button className="close-button" onClick={onClose}>X</button>
+        {children}
       </div>
     </div>
   );
 }
-
-export default Navbar;
